c97fc0cce79f1a89dd2adb365b57da6033d74c0e
dmacompton <d.mihaylov@ringostat.com>
2016-02-24 08:36:53 +0200

    11 finish

--- 2/src/js/client.js
+++ 2/src/js/client.js
@@ -9,13 +9,12 @@ import Settings from "./pages/Settings";
 const app = document.getElementById('app');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-      <Route path="/" component={Layout}>
-          <IndexRoute component={Featured}></IndexRoute>
-          <Route path="archives" component={Archives}></Route>
-          <Route path="settings" component={Settings}></Route>
-      </Route>
-  </Router>,
-app);
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+            <IndexRoute component={Featured}></IndexRoute>
+            <Route path="archives(/:article)" name="archives" component={Archives}></Route>
+            <Route path="settings" name="settings" component={Settings}></Route>
+        </Route>
+    </Router>, app);
 
 
--- 2/src/js/pages/Layout.js
+++ 2/src/js/pages/Layout.js
@@ -1,21 +1,32 @@
 import React from "react";
 import {Link} from "react-router";
 
-export default class Layout extends React.Component {
-  navigate() {
-    this.props.history.replace( "/");
-    // this.props.history.push("/");
-  }
+import Footer from "../components/layout/Footer";
+import Nav from "../components/layout/Nav";
 
-  render() {
-    return (
-      <div>
-        <h1>KillerNews.net</h1>
-        {this.props.children}
-        <Link to="archives" className="btn btn-danger">archives</Link>
-        <Link to="settings"><button className="btn btn-success">settings</button></Link>
-        <button onClick={this.navigate.bind(this)}>featured</button>
-      </div>
-    );
-  }
+export default class Layout extends React.Component {
+    render() {
+        const { location } = this.props;
+        const containerStyle = {
+            marginTop: "60px"
+        };
+        
+        return (
+            <div>
+                <Nav location={location} />
+        
+                <div class="container" style={containerStyle}>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>KillerNews.net</h1>
+                    
+                            {this.props.children}
+                        </div>
+                    </div>
+                </div>
+                
+                <Footer/>
+            </div>
+        );
+    }
 }
